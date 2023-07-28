9fd28e3d231de922c4bbba9e5aace99744a697a3
Joseph Sunil <josephs@packt.com>
2023-07-28 08:23:57 +0530

    Deleted files

--- CtoF.py
+++
@@ -1,7 +0,0 @@
-celsius=input("Enter the temperature in Celsius:")
-
-celsius = float(celsius)
-
-fahrenheit = (celsius * 1.8) + 32 
-
-print(f"{celsius} Celsius is {fahrenheit} Fahrenheit")
--- GroupNameGen.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to Josu's Group Name Generator")
-color = input("What is your favorite color?")
-animal = input("What is your favorite animal?")
-
-print(f"Your group name could be {color} {animal}")
